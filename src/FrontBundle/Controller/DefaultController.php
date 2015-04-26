814244960618560262628107e695477448b7c23d
Théo FIDRY <theo.fidry@gmail.com>
2015-04-26 11:40:15 +0200

    Test.

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace FrontBundle\Controller;
 
+use Google_Auth_AppIdentity;
+use Guzzle\Service\Client;
 use HappyR\Google\ApiBundle\Services\GoogleClient;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -20,11 +22,23 @@ class DefaultController extends Controller
 
         /** @var GoogleClient $clientService */
         $clientService = $this->get('happyr.google.api.client');
-        $clientService->getGoogleClient()->addScope('https://www.googleapis.com/auth/admin.directory.group');
+
+        $auth = new Google_Auth_AppIdentity($clientService->getGoogleClient());
+        $token = $auth->authenticateForScope(\Google_Service_Directory::ADMIN_DIRECTORY_USER);
+
+        dump($token);
+        die;
+        
+        if (!$token) {
+            die("Could not authenticate to AppIdentity service");
+        }
+        $clientService->getGoogleClient()->setAuth($auth);
 
         $directoryService = new \Google_Service_Directory($clientService->getGoogleClient());
 
-        dump($clientService->getAccessToken());
+        $results = $directoryService->members->listMembers('CdP');
+
+        dump($results);
         die;
 
         return $this->render('FrontBundle:Default:layout.html.twig');
