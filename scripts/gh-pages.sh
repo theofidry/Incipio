1a85f611141854696bf2a306214eb42481e975c9
Théo FIDRY <theo.fidry@gmail.com>
2015-10-05 15:37:43 +0100

    Better bash script error reporting

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -6,6 +6,7 @@ Calling publish artefacts to GitHub Pages script.
 Reporters:
   name: ${GH_USER_NAME}
   email: ${GH_USER_EMAIL}
+
 EOF
 
 git clone "${GH_REMOTE}" gh-pages --single-branch --branch=gh-pages
@@ -14,7 +15,7 @@ git config user.name "${GH_USER_NAME}"
 git config user.email "${GH_USER_EMAIL}"
 rm -rf reports
 mv ../dist/reports .
-git add -all
+git add --all
 git commit --message --quiet "${GH_COMMIT_MESSAGE}"
 git push --force --quiet origin gh-pages
 
