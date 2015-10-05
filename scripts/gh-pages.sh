4aa4dbe2cc957d9ac1defeb73c3fe1dd0525df44
Théo FIDRY <theo.fidry@gmail.com>
2015-10-05 13:44:36 +0100

    Fix GitHub pages deploy script
    * Correct the remote command
    * Set remote URL if already exist and add if does not to avoid throwing fatal errors
    * Fix script file permissions

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 rev=$(git rev-parse --short HEAD)
+remote_url="https://user:${GH_TOKEN}@github.com/in6pio/Incipio.git"
 
 cd dist
 git init
 git config user.name "TravisBot"
 git config user.email "travisbot@incipio.fr"
-git rm origin
-git add origin https://user:${GH_TOKEN}@github.com/user/repo.git ${GH_HOME}
+if git remote | grep origin > /dev/null;
+then
+  git remote set-url origin "${remote_url}"
+else
+  git remote add origin "${remote_url}"
+fi
 
 git add .
 git commit -m "Rebuild pages at ${rev}"
